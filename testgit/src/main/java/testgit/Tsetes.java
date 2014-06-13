a793c0695ef6762976961f74ef665cc152a86072
deadshot123 <deadshot123@qq.com>
2014-06-14 01:53:38 +0800

    ewr

---
+++ testgit/src/main/java/testgit/Tsetes.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package testgit;
+
+/**
+ * @author Austin
+ *
+ */
+public class Tsetes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
